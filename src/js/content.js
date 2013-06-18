ee25e6ec910b065312821b93462ba095aafbb6a5
Dmitry Sorin <dsorin@yandex-team.ru>
2013-06-18 16:49:19 +0400

    Listen to History API event

--- src/js/content.js
+++ src/js/content.js
@@ -1,1 +1,16 @@
-chrome.runtime.sendMessage({action: "content", url: document.location.href});
+(function () {
+    var lastVisitedURL;
+
+    // send message on inserted
+    informBackend();
+
+    // listen to History API changes
+    window.addEventListener("popstate", informBackend, false);
+
+    function informBackend() {
+        if (lastVisitedURL !== document.location.href) {
+            lastVisitedURL = document.location.href;
+            chrome.runtime.sendMessage({action: "content", url: lastVisitedURL});
+        }
+    }
+})();
